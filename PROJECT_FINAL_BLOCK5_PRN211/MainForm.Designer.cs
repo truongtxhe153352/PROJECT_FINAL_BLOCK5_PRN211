d6b3913111de1b3d5e73d2aa4a0a957f3ecf6534
truongtxhe153352 <truongtxhe153352@fpt.edu.vn>
2023-04-16 23:14:34 +0700

    Main form

---
+++ PROJECT_FINAL_BLOCK5_PRN211/MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace PROJECT_FINAL_BLOCK5_PRN211
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(28, 37);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(878, 125);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(31, 40);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 49);
+            button1.TabIndex = 0;
+            button1.Text = "Products";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(386, 40);
+            button2.Name = "button2";
+            button2.Size = new Size(117, 49);
+            button2.TabIndex = 1;
+            button2.Text = "Categorys";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(560, 40);
+            button3.Name = "button3";
+            button3.Size = new Size(110, 49);
+            button3.TabIndex = 2;
+            button3.Text = "Users";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(198, 40);
+            button4.Name = "button4";
+            button4.Size = new Size(108, 49);
+            button4.TabIndex = 3;
+            button4.Text = "Customers";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(736, 40);
+            button5.Name = "button5";
+            button5.Size = new Size(113, 49);
+            button5.TabIndex = 4;
+            button5.Text = "Orders";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(940, 450);
+            Controls.Add(panel1);
+            Name = "MainForm";
+            Text = "MainForm";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+    }
+}
---
+++ PROJECT_FINAL_BLOCK5_PRN211/MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJECT_FINAL_BLOCK5_PRN211
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
