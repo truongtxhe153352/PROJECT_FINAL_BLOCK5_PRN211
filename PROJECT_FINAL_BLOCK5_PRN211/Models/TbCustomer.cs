4ca9e4a27a8430bb58ab010344ebb62b43b2ff30
truongtxhe153352 <truongtxhe153352@fpt.edu.vn>
2023-04-16 22:57:40 +0700

    setting config

---
+++ PROJECT_FINAL_BLOCK5_PRN211/Models/TbCustomer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PROJECT_FINAL_BLOCK5_PRN211.Models
+{
+    public partial class TbCustomer
+    {
+        public TbCustomer()
+        {
+            TbOrders = new HashSet<TbOrder>();
+        }
+
+        public int Cid { get; set; }
+        public string? Cname { get; set; }
+        public string? Cphone { get; set; }
+
+        public virtual ICollection<TbOrder> TbOrders { get; set; }
+    }
+}
---
+++ PROJECT_FINAL_BLOCK5_PRN211/Models/TbOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PROJECT_FINAL_BLOCK5_PRN211.Models
+{
+    public partial class TbOrder
+    {
+        public int Orderid { get; set; }
+        public DateTime? Odate { get; set; }
+        public int? Pid { get; set; }
+        public int? Cid { get; set; }
+        public int? Qty { get; set; }
+        public int? Price { get; set; }
+
+        public virtual TbCustomer? CidNavigation { get; set; }
+        public virtual TbProduct? PidNavigation { get; set; }
+    }
+}
